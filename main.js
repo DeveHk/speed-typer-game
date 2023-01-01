9a98c69fe7bdd40426e86597a5d1ae0fd6b250e7
HARSH KUMAR <harshkr202002@outlook.com>
2023-01-01 11:38:04 +0530

    inter face complete

---
+++ main.js
@@ -0,0 +1,25 @@
+import './style.css'
+let navHide = false
+document.querySelector(".nav-hide").addEventListener('click', () => {
+  if (navHide) {
+    document.querySelector(".nav").classList.remove("nav-none")
+    document.querySelector(".nav-hide").classList.remove('nav-hide-rot')
+  }
+  else {
+    document.querySelector(".nav").classList.add("nav-none")
+    document.querySelector(".nav-hide").classList.add("nav-hide-rot")
+  }
+  navHide = !navHide
+})
+
+let scoreHide = true
+let rulehide = true
+document.querySelector("#scores-btn").addEventListener('click', () => {
+    document.querySelector(".scores-overlay").classList.add("show")
+    document.querySelector('.close-btn').addEventListener('click', ()=>{
+      document.querySelector(".scores-overlay").classList.remove("show")
+      scoreHide = !scoreHide
+    })
+    scoreHide = !scoreHide
+})
+
